c7149526167e46d0d3346304cb5d4e0cab380abd
Angelica Perez <aperez9423@gmail.com>
2020-08-14 21:45:07 -0400

    worked through most of the sprint challenge MVP requirements

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,9 +1,35 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+    const {queryByText} = render(<CheckoutForm/>);
+    const header = querybyText(/form/i);
+    expect(header).toBeInTheDocument();
+});
 
-test("form shows success message on submit with form details", () => {});
+test("form shows success message on submit with form details", () => {
+    const {getByLabelText, getByTestId} = render (<CheckoutForm/>)
+
+    const firstname = getByLabelText(/first name/i)
+    const lastname = getByLabelText(/last name/i)
+    const address = getByLabelText(/address/i)
+    const city = getByLabelText(/city/i)
+    const state = getByLabelText(/state/i)
+    const zipcode = getByLabelText(/zip/i)
+    const button = getByLabelText(/button/i)
+
+    fireEvent.change(firstname, {target: {value: "Angelica"}});
+    fireEvent.change(lastname, {target: {value: "Perez"}});
+    fireEvent.change(address, {target: {value: "321 Teach Avenue"}});
+    fireEvent.change(city, {target: {value: "Charlotte"}});
+    fireEvent.change(state, {target: {value: "North Carolina"}});
+    fireEvent.change(zipcode, {target: {value: "28269"}});
+    fireEvent.click(button);
+
+     const success = getByTestId(/successMessage/i)
+     expect(success).toBeInTheDocument();
+
+});
